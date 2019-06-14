359cb846ba3420fd12574846bea220de36ec2f1e
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-14 13:41:33 +0100

    first method added

--- resources/javascript/omdbjavascript.js
+++ resources/javascript/omdbjavascript.js
@@ -1,5 +1,16 @@
 // OMDB Javascript
 
+// READ - Get specific classroom (working)
 function getResult() {
+    let req = new XMLHttpRequest();
 
+    req.onload = function () {
+        let response = JSON.parse(req.responseText);
+        console.log(response);
+    }
+    let movieName = (document.getElementById("searchbox").value);
+    req.open("GET", "http://www.omdbapi.com/?t=" + movieName + "&apikey=a82907db");
+    req.send();
+
+    console.log(req);
 }
