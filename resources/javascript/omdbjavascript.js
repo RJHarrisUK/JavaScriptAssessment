e7368743a8ba312f95dc85ed1f3bcec694031786
RJHarrisUK <rjharrishome@yahoo.co.uk>
2019-06-14 15:08:29 +0100

    no longer appends constantly on search

--- resources/javascript/omdbjavascript.js
+++ resources/javascript/omdbjavascript.js
@@ -2,22 +2,30 @@
 
 // READ - Get specific classroom (working)
 function getResult() {
+
+    let Parent = document.getElementById("table");
+    while (Parent.hasChildNodes()) {
+        Parent.removeChild(Parent.firstChild);
+    }
+
     let req = new XMLHttpRequest();
 
     req.onload = function () {
         let response = JSON.parse(req.response);
 
-        console.log(response.Search);
 
-        let template = null;
+
+        console.log(response.Search);
+        let tableBuild = null;
         let len = response.Search.length;
+        $('table').clear;
         for (var i = 0; i < len; i++) {
 
-            template = '<tr><td>' + response.Search[i].Title
+            tableBuild = '<tr><td>' + response.Search[i].Title
                 + '</td><td>' + response.Search[i].Year
                 + '</td><td>' + response.Search[i].Type
                 + '</tr>'
-            $('table').append(template);
+            $('table').append(tableBuild);
         }
 
     }
